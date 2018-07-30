8e2382299c30f4c8c28b44d02e488605ac174e21
aditeesaxena96 <41783365+aditeesaxena96@users.noreply.github.com>
2018-07-30 12:12:21 +0530

    Delete shopping1.js

--- shopping1.js
+++
@@ -1,5 +0,0 @@
-var name=sessionStorage.getItem("billable_items");
-var TotalBill = sessionStorage.getItem("amt");
-let totaldata ='<td colspan="3" >'+name+'</td>';
-name+=totaldata;
-document.getElementById("show1").innerHTML=totaldata;
