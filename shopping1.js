2e57b1d9352a20a8ec3c3c6682d088910319c83e
aditeesaxena96 <41783365+aditeesaxena96@users.noreply.github.com>
2018-07-30 09:48:47 +0530

    Add files via upload

---
+++ shopping1.js
@@ -0,0 +1,5 @@
+var name=sessionStorage.getItem("billable_items");
+var TotalBill = sessionStorage.getItem("amt");
+let totaldata ='<td colspan="3" >'+name+'</td>';
+name+=totaldata;
+document.getElementById("show1").innerHTML=totaldata;
